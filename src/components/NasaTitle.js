6e98546db4533e9cca9059cb0196a2e37b1f537c
Zachary Taylor <ztay90@gmail.com>
2020-04-30 20:57:44 -0400

    Finished Part 2

--- src/components/NasaTitle.js
+++ src/components/NasaTitle.js
@@ -2,10 +2,17 @@ import React, { useState, useEffect} from "react";
 import axios from "axios";
 import NasaImg from "./NasaImg"
 import NasaExplanation from "./NasaExplanation"
+import {
+  Card, CardBody,
+  CardTitle, CardSubtitle, Button, Collapse
+} from 'reactstrap';
 
 export default function NasaTitle() {
 
   const [data, setData] = useState([])
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
   
   useEffect(() => {
     axios.get("https://api.nasa.gov/planetary/apod?api_key=8KGiNgztajHusdrqZYFjUpnKfnkia3TWQtg3LNJI")
@@ -18,17 +25,18 @@ export default function NasaTitle() {
   }, [])
   return (
     <div>
-      <h1>{data.title}</h1>
-      <h3>{data.date}</h3>
-      <NasaImg 
-        url={data.url}
-      />
-      <NasaExplanation 
-        explanation={data.explanation}
-      />
+      <Card className="card-container">
+        <CardBody>
+          <CardTitle className="photo-title">{data.title}</CardTitle>
+          <CardSubtitle className="photo-date">{data.date}</CardSubtitle>
+          <NasaImg url={data.url} />
+          <Button className="btn" color="danger" onClick={toggle} size="lg">Explanation</Button>
+          <Collapse isOpen={isOpen}>
+          <div className="card-info"><NasaExplanation explanation={data.explanation} /></div>
+          </Collapse>
+        </CardBody>
+      </Card>
     </div>
-    
-  )  
+  );
 }
 
-// export default NasaTitle;
